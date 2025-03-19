cae07e6639578a03538f0f56af2082c088ef326a
ester-a <esterandrews2@gmail.com>
2025-03-19 01:00:45 +0100

    added Petr page

--- ester/src/Petr.jsx
+++ ester/src/Petr.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import "./styles/main.scss";
-
+import cvImg from "./assets/cvImg.jpeg"
+import dkDentImg from "./assets/dkdentImg.jpeg"
 
 const Petr = () => {
   return (
     <>
+    <div className='peter-site'>
     <h1 className='petr-title'>Who is Petr?</h1>
     <div className='grid-container'>
         <div className='grid-item'>
-            <img className='project-img' src="" alt="" />
+            <a href='https://github.com/Pilsner12/dkdentalcare.git'><img className='project-img' src={dkDentImg} alt="DK Dental" /></a> 
         </div>
         <div className='grid-item'>
-            <img src="" alt="" />
+            <img className='project-img' src={cvImg} alt="CV Petr" />
         </div>
         <div className='grid-item'>
             <p><a className='link' href='https://dkdentalcare.netlify.app/'>DK Dental Care</a></p>
@@ -19,6 +21,7 @@ const Petr = () => {
         <div className='grid-item'>
             <p><a className='link' href='https://www.figma.com/design/f5J0e8h3maTpGaYNiQJrKq/PetrS-CV?node-id=0-1&p=f'>Petr CV</a></p>
         </div>
+     </div>
      </div>
     </>
   )
