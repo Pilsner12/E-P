4f5c7d927b636ce19f0ab6a227e5e81b6a447790
ester-a <esterandrews2@gmail.com>
2025-03-19 01:04:58 +0100

    x

--- ester/src/Petr.jsx
+++ ester/src/Petr.jsx
@@ -6,9 +6,10 @@ import dkDentImg from "./assets/dkdentImg.jpeg"
 const Petr = () => {
   return (
     <>
-    <div className='peter-site'>
-    <h1 className='petr-title'>Who is Petr?</h1>
+  
+  <h1 className='petr-title'>Who is Petr?</h1>
     <div className='grid-container'>
+    
         <div className='grid-item'>
             <a href='https://github.com/Pilsner12/dkdentalcare.git'><img className='project-img' src={dkDentImg} alt="DK Dental" /></a> 
         </div>
@@ -22,7 +23,7 @@ const Petr = () => {
             <p><a className='link' href='https://www.figma.com/design/f5J0e8h3maTpGaYNiQJrKq/PetrS-CV?node-id=0-1&p=f'>Petr CV</a></p>
         </div>
      </div>
-     </div>
+     
     </>
   )
 }
